249be5479d8f848deeeebbdacfc5c428888e5d2f
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2017-08-21 19:11:50 +0900

    Delete go-mode

--- init.el
+++ init.el
@@ -20,10 +20,6 @@
 (add-to-list 'auto-mode-alist '("\\.gradle\\'" . groovy-mode))
 (add-to-list 'auto-mode-alist '("\\.swift\\'" . swift-mode))
 
-;; go-mode
-;;
-(require 'go-mode-autoloads)
-
 ;; zsh-mode
 ;;
 (add-to-list 'auto-mode-alist '("\\.zsh_*" . sh-mode))
