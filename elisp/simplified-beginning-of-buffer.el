8cebc098c2e633d3c69a70dff0aa1a56645bb5aa
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2017-09-01 17:33:20 +0900

    Delete simplified-beginning-of-buffer.el

--- elisp/simplified-beginning-of-buffer.el
+++
@@ -1,6 +0,0 @@
-(defun simplified-beginning-of-buffer ()
-  "Move point to the beginning of the buffer;
-leave mark at previous position."
-  (interactive)
-  (push-mark)
-  (goto-char (point-min)))
